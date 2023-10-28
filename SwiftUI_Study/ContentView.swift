690e392e3263ba249e39b347ff49b37e4a2e2ff3
ciu <112779139+kangciu@users.noreply.github.com>
2023-10-28 18:07:05 +0900

    Initial Commit

---
+++ SwiftUI_Study/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Study
+//
+//  Created by 강치우 on 10/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Study/SwiftUI_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StudyApp.swift
+//  SwiftUI_Study
+//
+//  Created by 강치우 on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
