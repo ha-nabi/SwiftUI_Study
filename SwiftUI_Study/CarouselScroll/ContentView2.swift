2eb623a2ef6d9100fb85b69c8b56bd9198c1db80
ciu <112779139+kangciu@users.noreply.github.com>
2023-10-29 19:45:04 +0900

    AppSchool Gallery

---
+++ SwiftUI_Study/CarouselScroll/ContentView2.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView2.swift
+//  SwiftUI_Study
+//
+//  Created by 강치우 on 10/29/23.
+//
+
+import SwiftUI
+
+struct ContentView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView2()
+}
---
+++ SwiftUI_Study/CarouselScroll/Model/TripCard.swift
@@ -0,0 +1,18 @@
+//
+//  TripCard.swift
+//  SwiftUI_Study
+//
+//  Created by 강치우 on 10/29/23.
+//
+
+import SwiftUI
+
+struct TripCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TripCard()
+}
