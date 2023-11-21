fd46397e1c040d4ca194847ef0e83fc63b1c3ee6
ciu <112779139+kangciu@users.noreply.github.com>
2023-11-22 02:52:54 +0900

    kakao 소셜 로그인 구현

--- SwiftUI_Study/KaKao_Login/KaKao_Login/ContentView.swift
+++ SwiftUI_Study/KaKao_Login/KaKao_Login/ContentView.swift
@@ -8,17 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var kakaoAuthVM: KaKaoAuthVM = KaKaoAuthVM()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                Image("background")
+                    .resizable()
+                    .ignoresSafeArea(.all)
+                
+                VStack(spacing: 20) {
+                    
+                    Image("logo")
+                        .padding()
+                    
+                    Button {
+                        kakaoAuthVM.handleKakaoLogin()
+                    } label: {
+                        Image("kakao_login")
+                            .resizable()
+                            .frame(width: 277, height: 46)
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        Image("naver_login")
+                            .resizable()
+                            .frame(width: 277, height: 46)
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        Image("google_login")
+                            .resizable()
+                            .frame(width: 277, height: 46)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+// 로그아웃 버튼
+//Button {
+//    kakaoAuthVM.kakaoLogout()
+//} label: {
+//    Text("카카오 로그아웃")
+//}
--- SwiftUI_Study/KaKao_Login/KaKao_Login/KaKao_LoginApp.swift
+++ SwiftUI_Study/KaKao_Login/KaKao_Login/KaKao_LoginApp.swift
@@ -6,12 +6,38 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        return true
+    }
+}
 
 @main
-struct KaKao_LoginApp: App {
+struct YourApp: App {
+    
+    init() {
+        // Kakao SDK 초기화
+        KakaoSDK.initSDK(appKey: "178f4e1b9d68c29bb02c9ec498f42fde")
+        
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+                    .onOpenURL { url in // 뷰가 속한 Window에 대한 URL을 받았을 때 호출할 Handler를 등록하는 함수
+                        // 카카오 로그인을 위해 웹 혹은 카카오톡 앱으로 이동 후 다시 앱으로 돌아오는 과정을 거쳐야하므로, Handler를 추가로 등록해줌
+                        if AuthApi.isKakaoTalkLoginUrl(url) {
+                            _ = AuthController.handleOpenUrl(url: url)
+                        }
+                    }
+            }
         }
     }
 }
