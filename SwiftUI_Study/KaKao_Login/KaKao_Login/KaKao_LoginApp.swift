5cbf99cd18343be4d70956d9086db9733858b752
ciu <112779139+kangciu@users.noreply.github.com>
2023-11-21 23:32:51 +0900

    카카오 로그인 연습 중간 체크

---
+++ SwiftUI_Study/KaKao_Login/KaKao_Login/KaKao_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaKao_LoginApp.swift
+//  KaKao_Login
+//
+//  Created by 강치우 on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct KaKao_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
